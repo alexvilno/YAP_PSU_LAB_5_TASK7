16aefb604ffffde06ab3c5040ad2748724f87097
Artem Chashchin <92800788+alexvilno@users.noreply.github.com>
2022-12-16 12:06:33 +0500

    V1.1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using PSU_PL_LAB5_TASK_7;
 
 
@@ -10,8 +10,19 @@ class program
         var path4 = "/Users/alexvilno/Projects/PSU_PL_LAB5_TASK_7/PSU_PL_LAB5_TASK_7/task_4.txt";
         var path5 = "/Users/alexvilno/Projects/PSU_PL_LAB5_TASK_7/PSU_PL_LAB5_TASK_7/task_5.txt";
 
-        Solutions.TASK_1();
-        Solutions.TASK_2();
+        List<char> chars = new List<char>();
+        chars.Add('a');
+        chars.Add('b');
+        chars.Add('c');
+        LinkedList<char> chars1 = new LinkedList<char>();
+        chars1.AddFirst('a');
+        chars1.AddFirst('b');
+        chars1.AddFirst('a');
+        chars1.AddFirst('c');
+        chars1.AddFirst('d');
+
+        Solutions.TASK_1(ref chars);
+        Solutions.TASK_2(ref chars1);
         Solutions.TASK_3(path3);
         Solutions.TASK_4(path4);
         Solutions.TASK_5(path5);
